b5d4fb2e6bf7028ff4efa891514d4c9051ef0728
Hhall63 <harrison.hall@greensboro-nc.gov>
2026-07-10 10:01:22 -0400

    Add Save & Exit button to template editor

--- web/src/pages/TemplateEditorPage.jsx
+++ web/src/pages/TemplateEditorPage.jsx
@@ -127,6 +127,9 @@ export default function TemplateEditorPage() {
         <button className="primary" style={{ marginTop: 16 }} onClick={() => setEditingLine({})}>
           + Add Step
         </button>
+        <button className="secondary" style={{ marginTop: 12, width: "100%" }} onClick={() => navigate("/")}>
+          Save & Exit
+        </button>
       </div>
 
       {editingLine && (
